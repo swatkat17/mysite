f3cea422d7afeca1ad338700c572bedfa988c630
swatkat17 <102535780+swatkat17@users.noreply.github.com>
2024-01-23 05:02:23 +0530

    Update fragment.js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -43,7 +43,7 @@ export async function loadFragment(path) {
 
 export default async function decorate(block) {
   const link = block.querySelector('a');
-  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const path = https://main--mysite--swatkat17.hlx.page/block-collection/fragment-include.getAttribute('href') : block.textContent.trim();
   const fragment = await loadFragment(path);
   if (fragment) {
     const fragmentSection = fragment.querySelector(':scope .section');
